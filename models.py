59668333e96177d32151429da8e2c630e8598f15
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2024-04-02 09:56:46 -0400

    login and signup page set up

--- models.py
+++ models.py
@@ -1,2 +1,10 @@
 # Used for defining the simple in-memory data structures for lobbies and players
-lobbies = {}
+
+
+# key: email, value: {'first_name': str, 'last_name': str, 'password': str, 'player_id': str}
+users = {}  
+
+# key: lobby_id, value: {'holder_name': str, 'holder_id': str, 'limit': int}
+lobbies = {}  
+
+next_user_id = len(users) + 1  #auto-incrementing user IDs
